62b643bbff137f90e579d5fd2def38bc60163ccf
Frederick Giasson <fred@fgiasson.com>
2012-01-18 14:57:51 -0500

    First version of the structWSF upgrader

---
+++ structwsf-upgrade.sh
@@ -0,0 +1,168 @@
+
+STRUCTWSFVERSION="1.0a93"
+STRUCTWSFFOLDER="/usr/share/structwsf/"
+
+# From: http://ajdiaz.wordpress.com/2008/02/09/bash-ini-parser/
+cfg_parser ()
+{
+    ini="$(<$1)"                # read the file
+    ini="${ini//[/\[}"          # escape [
+    ini="${ini//]/\]}"          # escape ]
+    IFS=$'\n' && ini=( ${ini} ) # convert to line-array
+    ini=( ${ini[*]//;*/} )      # remove comments with ;
+    ini=( ${ini[*]/\    =/=} )  # remove tabs before =
+    ini=( ${ini[*]/=\   /=} )   # remove tabs be =
+    ini=( ${ini[*]/\ =\ /=} )   # remove anything with a space around =
+    ini=( ${ini[*]/#\\[/\}$'\n'cfg.section.} ) # set section prefix
+    ini=( ${ini[*]/%\\]/ \(} )    # convert text2function (1)
+    ini=( ${ini[*]/=/=\( } )    # convert item to array
+    ini=( ${ini[*]/%/ \)} )     # close array parenthesis
+    ini=( ${ini[*]/%\\ \)/ \\} ) # the multiline trick
+    ini=( ${ini[*]/%\( \)/\(\) \{} ) # convert text2function (2)
+    ini=( ${ini[*]/%\} \)/\}} ) # remove extra parenthesis
+    ini[0]="" # remove first element
+    ini[${#ini[*]} + 1]='}'    # add the last brace
+    eval "$(echo "${ini[*]}")" # eval the result
+}
+ 
+cfg_writer ()
+{
+    IFS=' '$'\n'
+    fun="$(declare -F)"
+    fun="${fun//declare -f/}"
+    for f in $fun; do
+        [ "${f#cfg.section}" == "${f}" ] && continue
+        item="$(declare -f ${f})"
+        item="${item##*\{}"
+        item="${item%\}}"
+        item="${item//=*;/}"
+        vars="${item//=*/}"
+        eval $f
+        echo "[${f#cfg.section.}]"
+        for var in $vars; do
+            echo $var=\"${!var}\"
+        done
+    done
+}
+
+# From: http://tldp.org/LDP/abs/html/colorizing.html
+# Colorizing the installation process.
+
+black='\E[1;30;40m'
+red='\E[1;31;40m'
+green='\E[1;32;40m'
+yellow='\E[1;33;40m'
+blue='\E[1;34;40m'
+magenta='\E[1;35;40m'
+cyan='\E[1;36;40m'
+white='\E[1;37;40m'
+
+cecho ()                     # Color-echo.
+                             # Argument $1 = message
+                             # Argument $2 = color
+{
+  local default_msg="No message passed."
+                             # Doesn't really need to be a local variable.
+
+  message=${1:-$default_msg}   # Defaults to default message.
+  color=${2:-$white}           # Defaults to white, if not specified.
+
+  echo -e "$color"
+  echo -e "$message"
+  
+  tput sgr0                     # Reset to normal.
+
+  return
+}
+
+echo -e "\n\n"
+cecho "----------" 
+cecho " Welcome! "
+cecho "----------"
+echo -e "\n\n"
+
+cecho "You are about to upgrade structWSF on your system..\n"
+
+cecho "  -> Everything that appears in **cyan** in the installation process, are important remarks and things to consider" $cyan
+cecho "  -> Everything that appears in **purple** in the installation process, are questions requested by the installation manual." $magenta
+cecho "  -> Everything that appears in **white** in the installation process, are processed undertaken by the installation script." $green
+cecho "  -> Everything that appears in **green** in the installation process, are processes that successfully ended." $green
+cecho "  -> Everything that appears in **yellow** in the installation process, are things that may have gone wrong and that needs investigation." $yellow
+cecho "  -> Everything that appears in **red** in the installation process, are things that goes wrong and that needs immediate investigation in order to OSF to operate normally." $red
+echo "  -> Everything else are processed ran by the installation script but that are performed by external installation programs and scripts."
+cecho "\n\nCopyright 2008-12. Structured Dynamics LLC. All rights reserved.\n\n"
+
+echo -e "\n\n"
+cecho "---------------------"
+cecho " Upgrading structWSF "
+cecho "---------------------"
+echo -e "\n\n"
+
+cecho "What is the new version of structWSF you want to install (latest: $STRUCTWSFVERSION):" $magenta
+
+read NEWSTRUCTWSFVERSION
+
+[ -n "$NEWSTRUCTWSFVERSION" ] && STRUCTWSFVERSION=$NEWSTRUCTWSFVERSION
+
+cecho "Where is located your current installation of structWSF (default: $STRUCTWSFFOLDER):" $magenta
+
+read NEWSTRUCTWSFFOLDER
+
+[ -n "$NEWSTRUCTWSFFOLDER" ] && STRUCTWSFFOLDER=$NEWSTRUCTWSFFOLDER
+
+# Make sure there is no trailing slashes
+STRUCTWSFFOLDER=$(echo "${STRUCTWSFFOLDER}" | sed -e "s/\/*$//")
+
+
+# Read the version of the currently installed structWSF version
+
+if [ -d "$STRUCTWSFFOLDER" ]; then
+  cfg_parser $STRUCTWSFFOLDER"/VERSION.ini"
+else
+  cecho "The structWSF Folder you provided is not existing on this system. Quitting..." $red
+  exit
+fi
+
+cfg.section.version
+
+CURRENTSTRUCTWSFVERSION=$version
+
+# Parse the versions file.
+
+cfg_parser "versions/versions.ini"
+
+# Parse the section for the current version of structWSF
+
+# Check if the current version is up-to-date
+
+cfg.section.$CURRENTSTRUCTWSFVERSION
+
+if [[ $nextVersion = "none" ]]
+then
+  cecho "Your structWSF version is already up-to-date" $cyan
+  exit 0
+fi
+
+UPGRADEVERSIONS=1
+
+while [ $UPGRADEVERSIONS = 1 ]; do
+
+  cecho "Upgrade to version: $nextVersion"
+
+  cfg.section.$nextVersion
+  
+  if [[ $updateScript != "" ]]
+  then
+    chmod 755 "versions/"$updateScript
+    "versions/"$updateScript $STRUCTWSFFOLDER
+  fi
+
+  if [[ $nextVersion = none || $nextVersion = $STRUCTWSFVERSION ]]
+  then
+    UPGRADEVERSIONS=0
+  fi  
+done 
+
+cecho "structWSF successfully upgraded to version $STRUCTWSFVERSION" $green
+
+
---
+++ versions/v1_0a91_to_v1_0a92.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+#
+# This version upgrade script is a shell that explains how an upgrade script should be
+# developed. It also includes a list of steps to perform depending on the kind of upgrade
+# necessary for a specific upgrade of a version A to a version B.
+#
+# NOTE: this first upgrade script is just in that package for demonstration purposes only
+#       the first structWSF version that can be upgraded is 1.0a92
+#
+
+STRUCTWSFVERSION="1.0a92"
+
+STRUCTWSFFOLDER=$1
+
+STRUCTWSFDOWNLOADURL="https://github.com/downloads/structureddynamics/structWSF-Open-Semantic-Framework/structWSF-v$STRUCTWSFVERSION.zip"
+
+# From: http://tldp.org/LDP/abs/html/colorizing.html
+# Colorizing the installation process.
+
+black='\E[1;30;40m'
+red='\E[1;31;40m'
+green='\E[1;32;40m'
+yellow='\E[1;33;40m'
+blue='\E[1;34;40m'
+magenta='\E[1;35;40m'
+cyan='\E[1;36;40m'
+white='\E[1;37;40m'
+
+cecho ()                     # Color-echo.
+                             # Argument $1 = message
+                             # Argument $2 = color
+{
+  local default_msg="No message passed."
+                             # Doesn't really need to be a local variable.
+
+  message=${1:-$default_msg}   # Defaults to default message.
+  color=${2:-$white}           # Defaults to white, if not specified.
+
+  echo -e "$color"
+  echo -e "$message"
+  
+  tput sgr0                     # Reset to normal.
+
+  return
+}
+
+cecho "\n\nBackuping the current version's files...\n"
+
+sudo cp -af $STRUCTWSFFOLDER"/" "/tmp/structWSF-backup-"$STRUCTWSFPREVIOUSVERSION"/"
+
+sudo mkdir upgrade
+
+cd upgrade
+
+cecho "\n\nDownload structWSF...\n"
+
+sudo wget $STRUCTWSFDOWNLOADURL  
+
+cecho "\n\nDecompressing structWSF...\n"
+
+sudo unzip "structWSF-v$STRUCTWSFVERSION.zip"  
+
+cd `ls -d structureddynamics*/`
+
+sudo mv -f * ../
+
+cd ../
+
+sudo rm -rf `ls -d structureddynamics*/`
+
+cecho "\n\nRemove default settings...\n"
+
+sudo rm data.ini
+sudo rm network.ini
+sudo rm framework/WebService.php
+sudo rm index.php
+sudo rm auth/wsf_indexer.php
+sudo rm scones/config.ini
+sudo rm "structWSF-v$STRUCTWSFVERSION.zip"
+
+cecho "\n\nMove new files to the current structWSF installation folder...\n"
+
+sudo cp -af * $STRUCTWSFFOLDER"/"
+
+#
+# These are the steps that needs to be performed for each upgrade.
+# Some of these steps may not apply to a specific version upgrade
+# in which case it will simply be ignored.
+#
+
+# 1) Delete files in the previous version of structWSF that are not needed anymore
+# 2) If the WebService.php file got modified, do re-create it using the same $data_ini and $network_ini settings
+# 3) If new data.ini settings got added, add them to the end of the current data.ini file
+# 4) If new network.ini settings got added, add them to the end of the current network.ini file
+# 5) If changes have been made to the Triple Store, do perform these changes
+# 6) If the Solr schema changed, instruct the user what to do (upgrading the schema, and then reloading its data)
+# 7) If new software or libraries are needed for this upgrade, then simply install and configure them.
+
+cd ..
+rm -rf upgrade
+
+cecho "\n\nThe files of the previous version are still available in that folder: /tmp/structWSF-backup-"$STRUCTWSFPREVIOUSVERSION"/\n" $green
+
